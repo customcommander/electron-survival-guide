8cd0051a1ce826b51039511203ce64328d8662d0
customcommander <hello@spinjs.com>
2021-12-17 09:40:16 +0000

    docs: add code intro for howtos/02/renderer.js

--- howtos/02/renderer.js
+++ howtos/02/renderer.js
@@ -1,4 +1,6 @@
 /**
+This code will eventually run in the renderer process but not as is.
+This module will get bundled by Webpack into a browser-compatible form.
 **/
 const answer = require('./answer');
 
